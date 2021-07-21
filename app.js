4a509753acbc5309c2784b3dcbbca3d07dc9f056
ericpuff <87716048+ericpuff@users.noreply.github.com>
2021-07-21 14:27:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function formatAndSendTweet(event) {
 
     console.log(tweetText);
 
-    return tweet.handleDupesAndTweet(tokenName, tweetText, image, formattedEthPrice);
+    return tweet.handleDupesAndTweet(tokenName, tweetText, image);
 }
 
 // Poll OpenSea every minute & retrieve all sales for a given collection in the last minute
